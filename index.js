eaba9eb6197fa14b160bb5c2d25eb27923cb019f
gzj <guanzhenjun>
2022-05-16 14:51:45 +0800

    第三次

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @Author: 关振俊
  * @Date: 2022-05-16 14:46:47
  * @LastEditors: 关振俊
- * @LastEditTime: 2022-05-16 14:47:01
+ * @LastEditTime: 2022-05-16 14:51:36
  * @Description: 
  */
 const a = '123'
 console.log(a)
+const b = '456'
