3e7f86f43cf02edc4355e01c8a554558d65ecff2
gzj <guanzhenjun>
2022-05-16 14:58:31 +0800

    dev提交

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
  * @Author: 关振俊
  * @Date: 2022-05-16 14:46:47
  * @LastEditors: 关振俊
- * @LastEditTime: 2022-05-16 14:47:01
+ * @LastEditTime: 2022-05-16 14:58:19
  * @Description: 
  */
 const a = '123'
+console.log('dev')
