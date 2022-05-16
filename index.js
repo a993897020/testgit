cecd917b923deb5dd66318602b6d56035bb4be11
gzj <guanzhenjun>
2022-05-16 14:51:53 +0800

    Revert "第三次"
    This reverts commit eaba9eb6197fa14b160bb5c2d25eb27923cb019f.

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
  * @Author: 关振俊
  * @Date: 2022-05-16 14:46:47
  * @LastEditors: 关振俊
- * @LastEditTime: 2022-05-16 14:51:36
+ * @LastEditTime: 2022-05-16 14:47:01
  * @Description: 
  */
 const a = '123'
 console.log(a)
-const b = '456'
