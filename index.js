b589f889fc1702a8703082df1de35b41a77c914c
gzj <guanzhenjun>
2022-07-14 09:41:18 +0800

    add

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
  * @Author: 关振俊
  * @Date: 2022-05-16 14:46:47
  * @LastEditors: 关振俊
- * @LastEditTime: 2022-05-16 14:58:19
+ * @LastEditTime: 2022-07-13 17:52:30
  * @Description: 
  */
 const a = '123'
 console.log(a)
 console.log('dev')
+console.log(666666)
