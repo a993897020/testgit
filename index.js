35bd6f6dfca3863d8092bf949fb0156a20d11050
gzj <guanzhenjun>
2022-08-23 16:07:51 +0800

    add: 水波纹

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-/*
- * @Author: 关振俊
- * @Date: 2022-05-16 14:46:47
- * @LastEditors: 关振俊
- * @LastEditTime: 2022-07-13 17:52:30
- * @Description: 
- */
+
 const a = '123'
 console.log(a)
 console.log('dev')
